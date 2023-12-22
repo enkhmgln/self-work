9b1b1f357b605562757292d60d68907e5dd40ca4
enkhmgln <sad64137@gmail.com>
2023-12-22 11:34:03 +0800

    prod

--- src/components/Benefit/index.js
+++ src/components/Benefit/index.js
@@ -7,10 +7,10 @@ import img3 from '@/assets/images/NewHOME.jpg'
 const Benefit = () => {
     return (<div className=" flex p-4  flex-col md:flex-row px-2 bg-blue-50">
 
-        <BenefitCard title={'Hello '} text={'description'} img={img}/>
-        <BenefitCard title={'Hello '} text={'description'} img={img1}/>
-        <BenefitCard title={'Hello '} text={'description'} img={img2}/>
-        <BenefitCard title={'Hello '} text={'description'} img={img3}/>
+        <BenefitCard title={'More flexibility '} text={'No blackout dates. Combine Cash & Points'} img={img}/>
+        <BenefitCard title={'More simplicity '} text={'Fixed points redemption value of 15 Points to US$1'} img={img1}/>
+        <BenefitCard title={'More privileges '} text={'Exclusive member rate and monthly offers'} img={img2}/>
+        <BenefitCard title={'More recognition '} text={'Earn status when you stay, dine, or shop with us'} img={img3}/>
 
 
     </div>);
--- src/components/BenefitCard/index.js
+++ src/components/BenefitCard/index.js
@@ -3,14 +3,14 @@ import Image from "next/image";
 const BenefitCard = ({img, title, text}) => {
 
     return (
-            <div className="w-full my-4 md:m-2 bg-blue-200">
+            <div className="w-full my-4 md:m-2 border-2 shadow-2xl">
             <div className=" flex flex-col items-center ">
                 <div className={'mb-2'}>
-                    <Image src={`${img.src}`} alt={'picture'} width={1000} height={100} className={'h-96 w-full rounded'}/>
+                    <Image src={`${img.src}`} alt={'picture'} width={1000} height={100} className={'h-96 w-full '}/>
                 </div>
-                <div className="mb-4">
-                    <h1 className="text-2xl font-semibold">{title}</h1>
-                    <p className="text-sm ">
+                <div className="mb-4 text-start px-4">
+                    <h1 className="text-2xl">{title}</h1>
+                    <p className="text-sm font-extralight font-mono ">
                         {text}
                     </p>
                 </div>
